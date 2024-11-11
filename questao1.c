0cfd2d57a87140c813a8b3a76bb8c87f759fef1b
Evellin Layanna <72310367+evellinlayanna@users.noreply.github.com>
2024-11-10 21:26:47 -0300

    Reorganização dos arquivos

--- questao1.c
+++ questao1.c
@@ -1,27 +1,21 @@
-// // Cálculo de Fatorial: Crie um programa que calcule e imprima o fatorial de
-// um
-// // número inteiro fornecido pelo usuário.
+// Cálculo de Fatorial: Crie um programa que calcule e imprima o fatorial de um número inteiro fornecido pelo usuário.
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//   int num, i;
-//   unsigned long long fatorial = 1; // unsigned long long é o tipo de dado que
-//                                    // armazena números inteiros de maior
-//                                    tamanho
+int main() {
+  int num, i;
+  unsigned long long fatorial = 1; unsigned long long // é o tipo de dado que armazena números inteiros de maior tamanho
 
-//   printf("Digite um número inteiro não negativo: ");
-//   scanf("%d", &num);
+  printf("Digite um número inteiro não negativo: ");
+  scanf("%d", &num);
 
-//   if (num < 0) {
-//     printf("Não existe fatorial de número negativo.\n");
-//   } else {
-//     for (i = 1; i <= num; i++) {
-//       fatorial *= i; // fatorial = fatorial * i, multiplica o valor de
-//       fatorial
-//                      // por i e armazena o resultado em fatorial
-//     }
-//     printf("Fatorial de %d é %llu\n", num, fatorial);
-//   }
-//   return 0;
-// }
+  if (num < 0) {
+    printf("Não existe fatorial de número negativo.\n");
+  } else {
+    for (i = 1; i <= num; i++) {
+      fatorial *= i; fatorial = fatorial * i; //multiplica o valor de fatorial por i e armazena o resultado em fatorial
+    }
+    printf("Fatorial de %d é %llu\n", num, fatorial);
+  }
+  return 0;
+}
--- questao2.c
+++ questao2.c
@@ -1,23 +1,22 @@
-// // Soma de Números em um Array: Faça um programa que leia 5 números inteiros,
-// // armazene-os em um array e calcule a soma total desses números.
+// Soma de Números em um Array: Faça um programa que leia 5 números inteiros, armazene-os em um array e calcule a soma total desses números.
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//   int numeros[5]; // Declara um array de 5 elementos inteiros
-//   int i;
-//   int soma = 0;
+int main() {
+  int numeros[5];
+  int i;
+  int soma = 0;
 
-//   printf("Insira 5 números inteiros:\n");
-//   for (i = 0; i < 5; i++) {
-//     scanf("%d", &numeros[i]);
-//   }
+  printf("Insira 5 números inteiros:\n");
+  for (i = 0; i < 5; i++) {
+    scanf("%d", &numeros[i]);
+  }
 
-//   for (i = 0; i < 5; i++) {
-//     soma += numeros[i];
-//   }
+  for (i = 0; i < 5; i++) {
+    soma += numeros[i];
+  }
 
-//   printf("A soma dos números é: %d\n", soma);
+  printf("A soma dos números é: %d\n", soma);
 
-//   return 0;
-// }
+  return 0;
+}
